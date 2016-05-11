631ee653dee664ad71e2e5ef1f376bbca862f593
Aidan Pelisson <pelisa@rpi.edu>
2016-05-11 13:25:43 -0400

    edit MultithreadedServerTests.java to account for added test cases

--- src/hw09/test/MultithreadedServerTests.java
+++ src/hw09/test/MultithreadedServerTests.java
@@ -53,8 +53,9 @@ public class MultithreadedServerTests extends TestCase {
 
 	 }
 	 	  	 
-     //@Test
+
      /*
+     @Test
      public void testDoubleIndirection() throws IOException {
          // initialize accounts
          accounts = new Account[numLetters];
@@ -82,23 +83,48 @@ public class MultithreadedServerTests extends TestCase {
      public void testLHSReference throws IOException {
     	 // initialize accounts
     	 accounts = new Account[numLetters];
-         accounts[0] = new Account(48);  // A = 48
-         accounts[1] = new Account(1);   // B = 1
-         accounts[8] = new Account(29);  // H = 29
-         accounts[13] = new Account(8);  // M = 8
-         accounts[15] = new Account(9);  // O = 9
-         accounts[22] = new Account(13); // V = 13
+         accounts[0] = new Account(47);  // A = 47
+         accounts[1] = new Account(0);   // B = 0
+         accounts[3] = new Account(14);  // D = 14
+         accounts[7] = new Account(29);  // H = 29
+         accounts[12] = new Account(7);  // M = 7
+         accounts[14] = new Account(9);  // O = 9
+         accounts[21] = new Account(12); // V = 13
          
          MultithreadedServer.runServer("src/hw09/data/doubleIndirection", accounts);
          
          // assert correct account values
-         assertEquals("Account A differs",9,accounts[0].getValue());
-         assertEquals("Account B differs",51,accounts[1].getValue());
-         assertEquals("Account H differs",9,accounts[5].getValue());
-         assertEquals("Account M differs",42,accounts[9].getValue());
-         assertEquals("Account O differs",5,accounts[11].getValue());
-         assertEquals("Account V differs",37,accounts[16].getValue());
+         assertEquals("Account A differs",47,accounts[0].getValue());
+         assertEquals("Account B differs",0,accounts[1].getValue());
+         assertEquals("Account D differs",14,accounts[3].getValue());
+         assertEquals("Account H differs",29,accounts[7].getValue());
+         assertEquals("Account M differs",29,accounts[12].getValue());
+         assertEquals("Account O differs",22,accounts[14].getValue());
+         assertEquals("Account V differs",13,accounts[21].getValue());
     	 
+     }
+     */
+     
+     /*
+     @Test
+     public void testVerifyTransactionAbortException throws IOException {
+     	// initialize accounts
+     	accounts = new Account[numLetters];
+     	accounts[0] = new Account(34);  // A = 34
+     	accounts[1] = new Account(17);  // B = 17
+     	accounts[2] = new Account(9);   // C = 9
+     	accounts[3] = new Account(7);   // D = 7
+     	accounts[4] = new Account(12);  // E = 12
+     	
+     	MultithreadedServer.runServer("src/hw09/data/doubleIndirection", accounts);
+         
+         // assert correct account values
+         assertEquals("Account A differs",34,accounts[0].getValue());
+         assertEquals("Account B differs",43,accounts[1].getValue());
+         assertEquals("Account C differs",9,accounts[2].getValue());
+         assertEquals("Account D differs",62,accounts[3].getValue());
+         assertEquals("Account E differs",12,accounts[4].getValue());
+
      }
      */
      
